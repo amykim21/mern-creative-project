424892e3d94b3834ba19f5ca2a889081b727581d
amy.kim <amy.kim@wustl.edu>
2020-12-06 14:09:13 -0800

    items fetch woring on inspect

--- mern-planner/client/src/App.js
+++ mern-planner/client/src/App.js
@@ -1,11 +1,25 @@
+import React, { Component } from 'react';
 import './App.css';
+import Items from './components/items/items_comp';
 
-function App() {
-  return (
+// changed from function App() to class App extends Component
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello</h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+      return (
     <div className="App">
       <h1>Hello</h1>
+      <Items></Items>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ mern-planner/client/src/components/items/items_comp.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './items_comp.css';
+
+// citation: https://www.youtube.com/watch?v=v0t42xBIYIs&t=1159s
+class Items extends Component {
+    constructor() {
+        super();
+        this.state = {
+            items: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/api/items')
+        .then(res => res.json())
+        .then(items => this.setState({items: items}, () => console.log('Items fetched...', items)));
+    }
+
+    render() {
+        return (
+            <div>
+            <h2>Items</h2>
+            </div>
+        );
+    }
+  
+}
+
+export default Items;
+
+// end of citation
