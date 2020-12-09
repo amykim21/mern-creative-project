597df3f3b5dd9fec870f25a8c2b1eb1afba59990
amy.kim <amy.kim@wustl.edu>
2020-12-09 00:07:00 -0800

    dummy item can be added

--- mern-planner/client/src/components/items/items_comp.js
+++ mern-planner/client/src/components/items/items_comp.js
@@ -27,11 +27,15 @@ class Items extends Component {
         this.addItem = this.addItem.bind(this);
     }
 
+    // version that adds to a user document
     addItem() {
         // todo: create popup asking for name of the item
     
-        const body = { name: 'new item4' };
-        fetch('http://localhost:5000/api/items/insert', {
+        console.log(this.props.username);
+        const newItems = [...this.state.items];
+        newItems.push({ name: "test1" });
+        const body = { username: 'q', newItems: newItems };
+        fetch('/api/items/insert', { // http://localhost:5000/api/items/insert
           method: 'POST',
           body: JSON.stringify(body),
           headers: { 'Content-Type': 'application/json' },
@@ -43,20 +47,66 @@ class Items extends Component {
             this.setState({items: newItems});
             console.log('Items fetched...', item);
         });
-      }
+    }
+
+    // version with onSubmit
+    // addItem(e) {
+    //     e.preventDefault();
+    //     // todo: create popup asking for name of the item
+    
+    //     const body = { name: 'new item4' };
+    //     fetch('/api/items/insert', { // http://localhost:5000/api/items/insert
+    //       method: 'POST',
+    //       body: JSON.stringify(body),
+    //       headers: { 'Content-Type': 'application/json' },
+    //     }) // localhost part not necessary because of proxy in package.json
+    //     .then(res => res.json())
+    //     .then(item => {
+    //         let newItems = [...this.state.items];
+    //         newItems.push(item);
+    //         this.setState({items: newItems});
+    //         console.log('Items fetched...', item);
+    //     });
+    // }
+
+    // addItem() {
+    //     // todo: create popup asking for name of the item
+    
+    //     const body = { name: 'new item4' };
+    //     fetch('/api/items/insert', { // http://localhost:5000/api/items/insert
+    //       method: 'POST',
+    //       body: JSON.stringify(body),
+    //       headers: { 'Content-Type': 'application/json' },
+    //     }) // localhost part not necessary because of proxy in package.json
+    //     .then(res => res.json())
+    //     .then(item => {
+    //         let newItems = [...this.state.items];
+    //         newItems.push(item);
+    //         this.setState({items: newItems});
+    //         console.log('Items fetched...', item);
+    //     });
+    // }
 
     componentDidMount() {
-        fetch('/api/items') // localhost part not necessary because of proxy in package.json
-        .then(res => res.json())
-        .then(items => this.setState({items: items}, () => console.log('Items fetched...', items)));
+        // fetch('/api/items') // localhost part not necessary because of proxy in package.json
+        // .then(res => res.json())
+        // .then(items => this.setState({items: items}, () => console.log('Items fetched...', items)));
     }
 
     render() {
-        const { classes } = this.props;
+        // const { classes } = this.props;
 
         return (
             <div>
             <h2>Items</h2>
+            {/* <form onSubmit={this.addItem}>
+                <input
+                // value={todoText}
+                // onChange={(e) => setTodoText(e.target.value)}
+                type="text"
+                ></input>
+                <button type="submit">Add</button>
+            </form> */}
             <button onClick={this.addItem}>Add Item</button>
             <Grid
             container
@@ -67,7 +117,7 @@ class Items extends Component {
             >
                 {this.state.items.map(
                     item =>
-                    <Grid item xs={12}>
+                    <Grid key={item._id} item xs={12}>
                     <Item key={item._id} item={item}></Item>
                     {/* <Item className={classes.item} key={item._id} item={item}>{item.name} {item.date}</Item> */}
                     </Grid>
--- mern-planner/routes/api/items.js
+++ mern-planner/routes/api/items.js
@@ -1,35 +1,78 @@
 // citation: https://www.youtube.com/watch?v=5yTazHkDR4o&list=PLillGF-RfqbbiTGgA77tGO426V3hRF9iE&index=2
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+
+// const { User, Items } = require('../../models/User');
+
+router.post('/insert', (req, res) => {
+    const newItems = req.body.newItems;
+    // const user;
+    console.log("here: " + req.body.username);
+    User.findOne({username: req.body.username })
+    .then(user => {
+        user.items = newItems;
+        user.save().then(oldItems => res.json(oldItems));
+        // Items.findOne({userId: user._id })
+        // .then(oldItems => {
+        //     if(oldItems == null) {
+        //         // first time that this user is inserting an item
+        //         Items.create({
+        //             userId: user._id,
+        //             items: newItems
+        //         });
+        //     } else {
+        //         oldItems.items = newItems;
+        //         oldItems.save().then(items => res.json(items));
+        //     }
+        // })
+        // .catch(err => res.status(404).json({success: false}));
+        // res.json(newItems);    
+    })
+    .catch(err => res.status(404).json({success: false}));
+});
+
+
+// User Model
+// import { User, Items } from '../../models/User'; // new schema
+// !! SyntaxError: Cannot use import statement outside a module
 
 // Item Model
-const Item = require('../../models/Item');
+// const Item = require('../../models/Item');
+
+// insert to Items where userId = userId from req.body
+// router.post('/insert', (req, res) => {
+//     const { userId } = req.body;
+//     const newItem = new Item({
+//         name: req.body.name
+//     });
+// });
 
 // GET request to api/items
 // Get All Items
-router.get('/', (req, res) => {
-    Item.find()
-    .sort({ date: -1 })
-    .then(items => res.json(items))
-});
+// router.get('/', (req, res) => {
+//     Item.find()
+//     .sort({ date: -1 })
+//     .then(items => res.json(items))
+// });
 
 // POST request to api/items
 // Create an item
-router.post('/insert', (req, res) => { // Amy: changed '/' --> '/insert'
-    const newItem = new Item({
-        name: req.body.name // name comes from the body of the request
-    });
+// router.post('/insert', (req, res) => { // Amy: changed '/' --> '/insert'
+//     const newItem = new Item({
+//         name: req.body.name // name comes from the body of the request
+//     });
 
-    newItem.save().then(item => res.json(item));
-});
+//     newItem.save().then(item => res.json(item));
+// });
 
 // DELETE request to api/items/:id
 // Delete an item
-router.delete('/delete/:id', (req, res) => { // Amy : changed '/:id' --> '/delete/:id'
-    Item.findById(req.params.id)
-    .then(item => item.remove().then(() => res.json({success: true})))
-    .catch(err => res.status(404).json({success: false})); // try to delete id that doesn't exist
-});
+// router.delete('/delete/:id', (req, res) => { // Amy : changed '/:id' --> '/delete/:id'
+//     Item.findById(req.params.id)
+//     .then(item => item.remove().then(() => res.json({success: true})))
+//     .catch(err => res.status(404).json({success: false})); // try to delete id that doesn't exist
+// });
 
 module.exports = router;
 // end of citation
